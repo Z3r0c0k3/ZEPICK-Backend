b028b70010a8deb77c8834372eedb5cbd01b6e9d
Z3r0c0k3 <hackerj1128@gmail.com>
2025-02-17 21:38:14 +0900

    feat: added MCUUID authentication logic

--- README.md
+++ README.md
@@ -10,7 +10,11 @@
   - POST `{"uuid": "인게임 사용자 UUID", "ip": "인게임 사용자 IP주소"}`
 - Response
   - `{"status": "True", "code": "인증코드"}`
+    - 정상 로직.
   - `{"status": "False", "message": "Duplicate UUID found in another collection."}`
+    - 이미 존재하는 UUID가 다른 컬렉션에 존재할 때 발생.
+  - `{"status": "False", "message": "This user is not authorized or is not a valid user."}`
+    - 정품인증 유저가 아니거나 마인크래프트 사용자가 아닐 때 발생.
 
 ### `/verify/verify-code`
 
@@ -20,4 +24,6 @@
 
 - Response
   - `{"status": "False", "message": "No matching code found."}`
+    - 일치하는 코드가 없을 때 발생.
   - `{"status": "True"}`
+    - 정상 로직.
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, UUID4
 from pymongo import MongoClient
 import random
 import string
+import httpx
 
 app = FastAPI()
 
@@ -13,7 +14,7 @@ verify_codes_collection = db["verify-codes"]
 
 # Pydantic 모델 정의
 class CodeGenRequest(BaseModel):
-    uuid: str
+    uuid: UUID4
     ip: str
 
 class VerifyCodeRequest(BaseModel):
@@ -25,11 +26,25 @@ async def root():
 
 @app.post("/verify/code-gen")
 async def generate_code(request: CodeGenRequest):
+    # UUID str 형변환
+    request.uuid = str(request.uuid)
+
     # UUID 중복 확인
     for collection_name in db.list_collection_names():
         if collection_name != "verify-codes":
             if db[collection_name].find_one({"uuid": request.uuid}):
                 return {"status": "False", "message": "Duplicate UUID found in another collection."}
+
+    # 사용자 UUID 검증
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://playerdb.co/api/player/minecraft/{request.uuid}")
+        data = response.json()
+        if data["code"] == "minecraft.api_failure":
+            return {"status": "False", "message": "This user is not authorized or is not a valid user."}
+        elif data["code"] == "player.found":
+            username = data["data"]["player"]["username"]
+        else:
+            return {"status": "False", "message": "Unexpected error occurred."}
 
     # 랜덤 코드 생성 및 중복 확인
     while True:
@@ -42,7 +57,7 @@ async def generate_code(request: CodeGenRequest):
 
     # 새로운 컬렉션 생성 및 데이터 삽입
     new_collection = db[random_code]
-    new_collection.insert_one({"uuid": request.uuid, "ip": request.ip})
+    new_collection.insert_one({"uuid": request.uuid, "ip": request.ip, "username": username})
 
     return {"status": "True", "code": random_code}
 
